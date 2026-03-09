2ffe21a30cb6695ab2f5f9d1031a3e9dedd3902f
John_tasma <tasmacode@protonmail.com>
2026-03-09 11:31:30 -0300

    ajuste na proporção de janelade configs

--- src/ui/settings_dialog.py
+++ src/ui/settings_dialog.py
@@ -16,7 +16,7 @@ class SettingsDialog(QDialog):
         self.current_config = config_manager.config.copy()
         
         self.setWindowTitle("Configurações")
-        self.resize(600, 450) #a proporção dessa janela tava me incomodando 🫡👍
+        self.resize(840, 390) #a proporção dessa janela tava me incomodando 🫡👍 @JohnB : agora sim 
         self.setStyleSheet("background-color: #252526; color: #cccccc;")
 
         layout = QVBoxLayout(self)
